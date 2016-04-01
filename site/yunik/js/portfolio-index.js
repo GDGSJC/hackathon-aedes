164caad7b66985306fd4fff339fe491294b22109
Alex Vidotto <alexvidotto@gmail.com>
2016-04-01 15:19:36 -0300

    Add site

---
+++ site/yunik/js/portfolio-index.js
@@ -0,0 +1,80 @@
+(function ($, window, document, undefined) {
+
+    var gridContainer = $('#grid-container'),
+        filtersContainer = $('#filters-container');
+
+	// init cubeportfolio
+    gridContainer.cubeportfolio({
+
+        defaultFilter: '*',
+
+        animationType: 'slideDelay',
+
+        gapHorizontal: 5,
+
+        gapVertical: 5,
+
+        gridAdjustment: 'responsive',
+
+        caption: 'minimal',
+
+        displayType: 'bottomToTop',
+
+        displayTypeSpeed: 100,
+
+        // lightbox
+        lightboxDelegate: '.cbp-lightbox',
+        lightboxGallery: true,
+        lightboxTitleSrc: 'data-title',
+        lightboxShowCounter: true,
+
+        // singlePage popup
+        singlePageDelegate: '.cbp-singlePage',
+        singlePageDeeplinking: true,
+        singlePageStickyNavigation: true,
+        singlePageShowCounter: true,
+        singlePageCallback: function (url, element) {
+            // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+        },
+
+        // singlePageInline
+        singlePageInlineDelegate: '.cbp-singlePageInline',
+        singlePageInlinePosition: 'above',
+        singlePageInlineShowCounter: true,
+        singlePageInlineInFocus: true,
+        singlePageInlineCallback: function(url, element) {
+            // to update singlePageInline content use the following method: this.updateSinglePageInline(yourContent)
+        }
+    });
+
+    // add listener for filters click
+    filtersContainer.on('click', '.cbp-filter-item', function (e) {
+
+        var me = $(this), wrap;
+
+        // get cubeportfolio data and check if is still animating (reposition) the items.
+        if ( !$.data(gridContainer[0], 'cubeportfolio').isAnimating ) {
+
+            if ( filtersContainer.hasClass('cbp-l-filters-dropdown') ) {
+                wrap = $('.cbp-l-filters-dropdownWrap');
+
+                wrap.find('.cbp-filter-item').removeClass('cbp-filter-item-active');
+
+                wrap.find('.cbp-l-filters-dropdownHeader').text(me.text());
+
+                me.addClass('cbp-filter-item-active');
+            } else {
+                me.addClass('cbp-filter-item-active').siblings().removeClass('cbp-filter-item-active');
+            }
+
+        }
+
+        // filter the items
+        gridContainer.cubeportfolio('filter', me.data('filter'), function () {});
+
+    });
+
+    // activate counter for filters
+    gridContainer.cubeportfolio('showCounter', filtersContainer.find('.cbp-filter-item'));
+
+})(jQuery, window, document);
